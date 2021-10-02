3ea746113a106e33a3aa1ee109cef02b8cd94e38
fisher <fisher521.fs@gmail.com>
2021-10-02 18:38:26 -0500

    Add Challenge1 stub

---
+++ src/main/java/Challenge1.java
@@ -0,0 +1,12 @@
+public class Challenge1 {
+    /**
+     * Given an array of arrays of portion sizes, return the index with the greatest average portion size.
+     *
+     * @param restaurants A 2D array such that each element of the outer array is
+     *                      an array representing the portion sizes of a restaurant's entrees (in ounces).
+     * @return the index of the array with greatest average portion size.
+     */
+    public int largestPortions(double[][] restaurants) {
+        return 0;
+    }
+}
