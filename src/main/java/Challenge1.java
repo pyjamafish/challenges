a43d53f3bc7aa5b226f5d0821c7a8c40c866aaf3
fisher <fisher521.fs@gmail.com>
2021-10-02 18:40:18 -0500

    Change unit and type for portion sizes

--- src/main/java/Challenge1.java
+++ src/main/java/Challenge1.java
@@ -3,10 +3,10 @@ public class Challenge1 {
      * Given an array of arrays of portion sizes, return the index with the greatest average portion size.
      *
      * @param restaurants A 2D array such that each element of the outer array is
-     *                      an array representing the portion sizes of a restaurant's entrees (in ounces).
+     *                      an array representing the portion sizes of a restaurant's entrees (in grams).
      * @return the index of the array with greatest average portion size.
      */
-    public int largestPortions(double[][] restaurants) {
+    public int largestPortions(int[][] restaurants) {
         return 0;
     }
 }
