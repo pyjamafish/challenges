2d180ea607820d4b403b1e2cc097b92799cd29fa
fisher <fisher521.fs@gmail.com>
2021-10-02 19:41:53 -0500

    Add solution

--- src/main/java/Challenge1.java
+++ src/main/java/Challenge1.java
@@ -9,6 +9,38 @@ public class Challenge1 {
      * with the greater number of entrees.
      */
     public static int largestPortions(int[][] restaurants) {
-        return 0;
+        double greatestAverage = 0;
+        int greatestAverageIndex = 0;
+
+        // Loop through every restaurant
+        for (int i = 0, restaurantsLength = restaurants.length; i < restaurantsLength; i++) {
+            int[] currentRestaurant = restaurants[i];
+            double currentRestaurantAverage = getArrayAverage(currentRestaurant);
+
+            if (currentRestaurantAverage > greatestAverage) {
+                greatestAverage = currentRestaurantAverage;
+                greatestAverageIndex = i;
+            } else if (currentRestaurantAverage == greatestAverage) {
+                if (currentRestaurant.length > restaurants[greatestAverageIndex].length) {
+                    greatestAverage = currentRestaurantAverage;
+                    greatestAverageIndex = i;
+                }
+            }
+        }
+        return greatestAverageIndex;
+    }
+
+    /**
+     * Get the average of an array of integers.
+     *
+     * @param array The array to take the average of.
+     * @return The average (mean) of the array.
+     */
+    private static double getArrayAverage(int[] array) {
+        int sum = 0;
+        for (int number : array) {
+            sum += number;
+        }
+        return (double) sum / array.length;
     }
 }
